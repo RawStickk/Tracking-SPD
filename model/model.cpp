fc45fef185d6a1260c9f41817ab82d8958e079e7
RawStickk <rostislav.klimko@gmail.com>
2023-08-10 19:44:58 +0300

    Refactoring

--- model/model.cpp
+++ model/model.cpp
@@ -29,14 +29,21 @@ struct Particle {
 
 struct Hit {
     Wire pipe;
-    double R;
-    unsigned ntrk;
-    Point p;
+    double R; ///min dist from particle to wire
+    unsigned ntrk; /// number of track
+    Point p; ///particle momentum
 };
 
 void construct(vector<Wire> *pipes, double const wire_rad, double const wire_angle,
                double const R0, double const d,
                unsigned const N) {
+    /// Description: constructs the detector by placing wires in layers
+    /// Variables:  pipes - array of wires
+    ///             wire_rad - radius of a pipe
+    ///             wire_angle - incline angle
+    ///             R0 - radius of the inner layer (of smallest radius)
+    ///             d - distance between inclined layer and next parallel one)
+    ///             N - number of layers
 
     double R = R0, angle = 0;
 
@@ -58,6 +65,8 @@ void construct(vector<Wire> *pipes, double const wire_rad, double const wire_ang
 }
 
 void pipes_output(vector<Wire> const *pipes, unsigned const N) {
+    /// Description: outputs wires coordinates into csv file
+
     fstream fout;
     fout.open("wires.csv", ios::out | ios::trunc);
 
@@ -72,6 +81,8 @@ void pipes_output(vector<Wire> const *pipes, unsigned const N) {
 }
 
 void particle_parameters(Particle &particle, unsigned const num_particles, unsigned const seed) {
+    /// Description: generates randomly particle momentum components
+
     default_random_engine rng(seed);
     uniform_real_distribution<double> dstr(-500, 500);
     particle.p.x = dstr(rng);
@@ -92,10 +103,14 @@ Point vect_prod(Point const &a, Point const &b) {
 }
 
 Point dot(double const c, Point const p) {
+    ///Description: multiplies vector by scalar
+
     return {c * p.x, c * p.y, c * p.z};
 }
 
 double calc_angle(double const sine, double const cosine) {
+    ///Description: finds angle value using it's sine and cosine values
+
     double angle;
     if (abs(cosine) < 1e-9) {
         angle = asin(sine);
@@ -108,6 +123,9 @@ double calc_angle(double const sine, double const cosine) {
 }
 
 double dist(Point const &track_point, Wire const &pipe) {
+    ///Description: finds distance from a point to a line
+    ///by rotating coord system and calculating area of parallelogram
+
     double teta = calc_angle(pipe.point.y / sqrt(pipe.point.x * pipe.point.x + pipe.point.y * pipe.point.y),
                              pipe.point.x / sqrt(pipe.point.x * pipe.point.x + pipe.point.y * pipe.point.y));
 
@@ -119,28 +137,42 @@ double dist(Point const &track_point, Wire const &pipe) {
 }
 
 void hits_output(vector<Hit> const &hits) {
+    ///Description: outputs all the data characterizing hits
+    ///(num of track, wire coordinates and inclination angle, min dist between particle and wire, particle momentum)
+
     fstream fout;
     fout.open("hits.csv", ios::out | ios::trunc);
-    fout << "x,y,R,alpha,NTRK,p_x,p_y,p_z" << "\n";
+    fout << "NTRK,x,y,R,alpha,p_x,p_y,p_z" << "\n";
     for (unsigned i = 0; i < hits.size(); i++) {
-        fout << hits[i].pipe.point.x << "," << hits[i].pipe.point.y << "," << hits[i].R << "," << hits[i].pipe.alpha << ",";
-        fout << hits[i].ntrk << "," << hits[i].p.x << "," << hits[i].p.y << "," << hits[i].p.z << "\n";
+        fout << hits[i].ntrk << "," << hits[i].pipe.point.x << "," << hits[i].pipe.point.y << "," << hits[i].R << "," << hits[i].pipe.alpha << ",";
+        fout << hits[i].p.x << "," << hits[i].p.y << "," << hits[i].p.z << "\n";
     }
     fout.close();
 }
 
 void hit_detection(vector<Wire> const *pipes, unsigned const N, double const len, double const pipe_rad,
         Point const &B, Particle const &particle, unsigned const ntrk, vector<Hit> &hits) {
-    Point p_xy = {particle.p.x, particle.p.y, 0};
-    Point rho = dot(c * 1e-3 * modulus_(particle.p) / (particle.charge * modulus_(B) * modulus_(p_xy)), vect_prod(particle.p, B));
+    ///Description: detects hits for a particle
+    ///             1.iterates through layers and extrapolates particle track in their vicinity
+    ///             2.particle track is parametrized by azimuthal angle phi
+    ///             3.iterates through this angle, particle coordinates transfers to XYZ system
+    ///             and checks for hits by calc of dist between particle and wire
+    ///             (if it is less than pipe_rad -> hit)
+
+    Point p_xy = {particle.p.x, particle.p.y, 0}; ///perpendicular component of particle momentum to Z axis
+    Point rho = dot(c * 1e-3 * modulus_(particle.p) / (particle.charge * modulus_(B) * modulus_(p_xy)), vect_prod(particle.p, B)); ///helix radius
     double beta = calc_angle(particle.p.y / sqrt(modulus_(p_xy)), particle.p.x / sqrt(modulus_(p_xy)));
-
+    ///iteration through layers
     for (unsigned i = 0; i < N; i++) {
-        double R_i = sqrt(modulus_({pipes[i][0].point.x, pipes[i][0].point.y, 0})) - pipe_rad;
+        double R_i = sqrt(modulus_({pipes[i][0].point.x, pipes[i][0].point.y, 0})) - pipe_rad; ///radius of "i" layer in detector
 
-        if (R_i * R_i / modulus_(rho) > 4) break;
+        if (R_i * R_i / modulus_(rho) > 4) break; /// check whether a particle can reach this layer
+                                                  ///(in case of strong magnetic field or small momentum its track can be curved)
 
         double phi = acos(1 - R_i * R_i / (2 * modulus_(rho))), phi0;
+        ///inclined layers of small radius are not cylindrical near their ends even approximately
+        ///[double gap] is the gap between the end of a pipe and the inner layer
+        ///estimation of the gap and calculation of the ending value(phi0) of phi
         double *gap = new double;
         *gap = sqrt((len * pipes[i][0].alpha * pi / 180) * (len * pipes[i][0].alpha * pi / 180) + (R_i + pipe_rad) * (R_i + pipe_rad))
         - R_i + pipe_rad;
@@ -151,21 +183,22 @@ void hit_detection(vector<Wire> const *pipes, unsigned const N, double const len
             phi0 = acos(1 - (R_i + *gap) * (R_i + *gap) / (2 * modulus_(rho)));
         delete gap;
 
-        double const d_phi = (phi0 - phi) / 100;
-
+        double const d_phi = (phi0 - phi) / 100; ///step
+        ///in this part extrapolation process starts
         bool hit = false, out = false;
         Wire wire;
         double min_dist;
 
         while (phi < phi0) {
             Point r = {sqrt(modulus_(rho)) * sin(phi - beta) + rho.x, sqrt(modulus_(rho)) * cos(phi - beta) + rho.y,
-            sqrt(modulus_(rho)) * particle.p.z * phi / sqrt(modulus_(p_xy))};
-            if (r.z > len) {
+            sqrt(modulus_(rho)) * particle.p.z * phi / sqrt(modulus_(p_xy))}; ///current position of the particle in XYZ
+            if (r.z > len) { ///check whether particle has flown out through the end of detector
                 cout << i << " out \n";
                 out = true;
                 break;
             }
-
+            ///iteration through all the pipes in the layer till hit is not detected
+            ///by checking if the distance between particle ad wire is less than the pipe radius
             for (unsigned j = 0; j < pipes[i].size() && !hit; j++) {
                 if (dist(r, pipes[i][j]) < pipe_rad) {
                     hit = true;
@@ -174,7 +207,7 @@ void hit_detection(vector<Wire> const *pipes, unsigned const N, double const len
                     wire = pipes[i][j];
                 }
             }
-            if (dist(r, wire) < min_dist)
+            if (dist(r, wire) < min_dist) ///calculation of min dist between particle and wire
                 min_dist = dist(r, wire);
 
             phi += d_phi;
@@ -186,8 +219,8 @@ void hit_detection(vector<Wire> const *pipes, unsigned const N, double const len
 
 int main() {
     double const R0 = 100, d = 50,
-    pipe_rad = 5, len = 1000, wire_angle = 5;
-    unsigned const N = 40;
+    pipe_rad = 5, len = 1000, wire_angle = 5; ///angle is i deg
+    unsigned const N = 40; ///number of layers
     Point const B = {0, 0, 1}; /// in [T]
 
     vector<Wire> pipes[N];
